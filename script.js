d6df71e84dfacbbe5cf00238c7d1aa2fb0a230ea
VZNRY1 <tajbland1@gmail.com>
2022-11-30 22:04:34 -0500

    created the code for the html,css, and java files, still need to do the readme

---
+++ script.js
@@ -0,0 +1,113 @@
+function start() {
+    var show = document.querySelector(".hide");
+    console.log(show);
+    show.className = "grid";
+    var startButton = document.querySelector("#start-button");
+    startButton.className = "hide";
+    var nextButton = document.querySelector("#next-button");
+    nextButton.className = "grid";
+    setTime();
+    console.log("Updated fix");
+  }
+  
+  function nextQuestion() {
+    if (count < questions.length) {
+      count++;
+  
+      var question = questions[count];
+      var questionTag = document.querySelector("#question");
+      questionTag.innerHTML = question.Question;
+      var buttonTags = document.querySelector("#answer-buttons").children;
+      console.log(buttonTags);
+      var answer1 = buttonTags[0];
+      var answer2 = buttonTags[1];
+      var answer3 = buttonTags[2];
+      var answer4 = buttonTags[3];
+      answer1.innerHTML = question.Answer1;
+      answer2.innerHTML = question.Answer2;
+      answer3.innerHTML = question.Answer3;
+      answer4.innerHTML = question.Answer4;
+      console.log(count);
+    }
+  }
+  if (count === 3) {
+    var studentGrade = {
+      Grade: score,
+    };
+    localStorage.setItem("TotalScore", JSON.stringify(studentGrade));
+    var answerButtons = document.querySelector("#answer-buttons");
+    answerButtons.className = "hide";
+  }
+  var questions = [
+    {
+      Question: "Which is an HTML element?",
+      Answer1: "Blue",
+      Answer2: "Green",
+      Answer3: "Elephant",
+      Answer4: "Div",
+  
+      correctAnswer: "Div",
+    },
+    {
+      Question: "Which is a Coding Language?",
+      Answer1: "Japan",
+      Answer2: "Lava",
+      Answer3: "Reading",
+      Answer4: "CSS",
+  
+      correctAnswer: "CSS",
+    },
+    {
+      Question: "Name a Notepad?",
+      Answer1: "VS Code",
+      Answer2: "No. 2",
+      Answer3: "The Rock",
+      Answer4: "HTML",
+  
+      correctAnswer: "VS Code",
+    },
+  ];
+  var count = 0;
+  var nextButton = document.querySelector("#next-button");
+  nextButton.addEventListener("click", nextQuestion);
+  
+  //Function determining if answer is Correct
+  function selectAnswer(element) {
+    var selectedAnswer = element.innerHTML;
+    var question = questions[count];
+    if (question.correctAnswer === selectedAnswer) {
+      alert("Correct!");
+      score++;
+      var keepScore = document.querySelector("#Score");
+      keepScore.innerHTML = "Total Score " + (score / 3) * 100;
+    } else {
+      alert("Incorrect!");
+      secondsLeft = secondsLeft - 4;
+    }
+    console.log(selectedAnswer);
+    console.log(question.correctAnswer);
+  }
+  var timeEl = document.querySelector("#Time");
+  //Timer Function
+  var secondsLeft = 60;
+  
+  function setTime() {
+    var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left in quiz.";
+  
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+        var answerButtons = document.querySelector("#answer-buttons");
+        answerButtons.className = "hide";
+      }
+    }, 1000);
+  }
+  var buttonTags = document.querySelector("#answer-buttons").children;
+  console.log(buttonTags);
+  var answer1 = buttonTags[0];
+  var answer2 = buttonTags[1];
+  var answer3 = buttonTags[2];
+  var answer4 = buttonTags[3];
+  
+  var score = 0;
